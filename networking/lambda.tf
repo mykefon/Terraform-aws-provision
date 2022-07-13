0a2fe67674cb73601d96b1a7c84ed1e134150003
Myke Mike Fon <100591000+mykefon@users.noreply.github.com>
2022-07-13 00:04:27 -0400

    update

--- networking/lambda.tf
+++ networking/lambda.tf
@@ -2,7 +2,7 @@ module "lambda_function" {
   source = "terraform-aws-modules/lambda/aws"
 
   function_name = "lambdaST01"
-  description   = "My awesome lambda function"
+  description   = "Terraform lambda function"
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
   publish       = true
@@ -10,7 +10,7 @@ module "lambda_function" {
   source_path = "../src/lambda-function1"
 
   store_on_s3 = true
-  s3_bucket   = "terraform-s3-bucket-001"
+  s3_bucket   = "xxx-s3-bucket-001"
 
   layers = [
     module.lambda_layer_s3.lambda_layer_arn,
@@ -31,11 +31,11 @@ module "lambda_layer_s3" {
   create_layer = true
 
   layer_name          = "lambda-layer-s3"
-  description         = "My amazing lambda layer (deployed from S3)"
+  description         = "Deploying from s3"
   compatible_runtimes = ["python3.8"]
 
   source_path = "../src/lambda-layer"
 
   store_on_s3 = true
-  s3_bucket   = "terraform-s3-bucket-001"
+  s3_bucket   = "xxx-s3-bucket-001"
 }
