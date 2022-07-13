1f7cc83269246c96240fa247421a789bb892b367
Myke Mike Fon <100591000+mykefon@users.noreply.github.com>
2022-07-12 23:50:39 -0400

    update

--- networking/provider.tf
+++ networking/provider.tf
@@ -1,12 +1,15 @@
 terraform {
+  required_version = ">= 1.2.4"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.18.0"
+      version = ">= 4.9.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
